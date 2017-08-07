04c858872ba68fe605b4272826f7ebe91e295428
Steve Gordon <sgordon@hotmail.co.uk>
2017-08-07 18:48:09 +0100

    Title

--- build.sh
+++ build.sh
@@ -18,4 +18,4 @@ docker rmi dockerdemo-servicea
 docker rmi 865288682694.dkr.ecr.eu-west-2.amazonaws.com/dockerdemo-servicea:latest
 
 sudo aws ecs register-task-definition --cli-input-json file://./TaskDefinition.json
-sudo aws ecs update-service --cluster dockerdemo --service DockerDemo-ECSService-50IH1QGP2Z1N --task-definition dockerdemo-servicea
+sudo aws ecs update-service --cluster dockerdemo --service DockerDemo-ECSService-1EPIPUTIDTW9M --task-definition dockerdemo-servicea
