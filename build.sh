e9fa1339e50832e6c90daac1aa3fb647e0bcf667
Steve Gordon <sgordon@hotmail.co.uk>
2017-08-28 10:57:04 +0100

    Testing include of git versioning for demo

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 set -e
 
-docker build -t dockerdemo-servicea .
+docker build -t 865288682694.dkr.ecr.eu-west-2.amazonaws.com/dockerdemo-servicea:${GIT_COMMIT} .
 
 sudo aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 sudo aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
@@ -11,11 +13,10 @@ loginCommand=$(sudo aws ecr get-login --no-include-email --region eu-west-2)
 $loginCommand
 set -x
 
-docker tag dockerdemo-servicea 865288682694.dkr.ecr.eu-west-2.amazonaws.com/dockerdemo-servicea:latest
-docker push 865288682694.dkr.ecr.eu-west-2.amazonaws.com/dockerdemo-servicea:latest
+docker push 865288682694.dkr.ecr.eu-west-2.amazonaws.com/dockerdemo-servicea:${GIT_COMMIT}
+docker rmi 865288682694.dkr.ecr.eu-west-2.amazonaws.com/dockerdemo-servicea:${GIT_COMMIT}
 
-docker rmi dockerdemo-servicea
-docker rmi 865288682694.dkr.ecr.eu-west-2.amazonaws.com/dockerdemo-servicea:latest
+sed -e "s;%GIT_SHA%,${GIT_COMMIT};g" ./TaskDefinition.json > ./TaskDefinition-${GIT_COMMIT}.json
+sudo aws ecs register-task-definition --cli-input-json file://./TaskDefinition-${GIT_COMMIT}.json
 
-sudo aws ecs register-task-definition --cli-input-json file://./TaskDefinition.json
-sudo aws ecs update-service --cluster dockerdemo --service DockerDemo-ECSService-WY2VWF9YOM5F --task-definition dockerdemo-servicea
+sudo aws ecs update-service --cluster dockerdemo --service DockerDemo-ECSService-XO0L8XW5TCG1 --task-definition dockerdemo-servicea
