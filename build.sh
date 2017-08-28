c0a7db6a09c1d91323738b003e1769ecd1a78194
Steve Gordon <sgordon@hotmail.co.uk>
2017-08-28 11:17:37 +0100

    Short git sha test

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+
 set -e
 
-docker build -t 865288682694.dkr.ecr.eu-west-2.amazonaws.com/dockerdemo-servicea:${GIT_COMMIT} .
+GIT_COMMIT_SHORT=$(git rev parse --short HEAD)
+
+docker build -t 865288682694.dkr.ecr.eu-west-2.amazonaws.com/dockerdemo-servicea:${GIT_COMMIT_SHORT} .
 
 sudo aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 sudo aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
@@ -11,10 +15,10 @@ loginCommand=$(sudo aws ecr get-login --no-include-email --region eu-west-2)
 $loginCommand
 set -x
 
-docker push 865288682694.dkr.ecr.eu-west-2.amazonaws.com/dockerdemo-servicea:${GIT_COMMIT}
-docker rmi 865288682694.dkr.ecr.eu-west-2.amazonaws.com/dockerdemo-servicea:${GIT_COMMIT}
+docker push 865288682694.dkr.ecr.eu-west-2.amazonaws.com/dockerdemo-servicea:${GIT_COMMIT_SHORT}
+docker rmi 865288682694.dkr.ecr.eu-west-2.amazonaws.com/dockerdemo-servicea:${GIT_COMMIT_SHORT}
 
-sed -e "s/%GIT_SHA%/${GIT_COMMIT}/g" ./TaskDefinition.json > ./TaskDefinition-${GIT_COMMIT}.json
-sudo aws ecs register-task-definition --cli-input-json file://./TaskDefinition-${GIT_COMMIT}.json
+sed -e "s/%GIT_SHA%/${GIT_COMMIT_SHORT}/g" ./TaskDefinition.json > ./TaskDefinition-${GIT_COMMIT_SHORT}.json
+sudo aws ecs register-task-definition --cli-input-json file://./TaskDefinition-${GIT_COMMIT_SHORT}.json
 
 sudo aws ecs update-service --cluster dockerdemo --service DockerDemo-ECSService-XO0L8XW5TCG1 --task-definition dockerdemo-servicea
