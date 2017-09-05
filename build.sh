3751599eda65224ce39e19615ecfcc202f3d447c
Steve Gordon <sgordon@hotmail.co.uk>
2017-09-05 19:49:43 +0100

    Build file

--- build.sh
+++ build.sh
@@ -4,6 +4,10 @@ set -e
 
 docker build -t 865288682694.dkr.ecr.eu-west-2.amazonaws.com/dockerdemo-servicea:${GIT_COMMIT} .
 
+sudo aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+sudo aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+sudo aws configure set default.region eu-west-2
+
 set +x
 loginCommand=$(sudo aws ecr get-login --no-include-email --region eu-west-2)
 $loginCommand
