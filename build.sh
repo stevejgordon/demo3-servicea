f17f82b30e30eb5786da4ff8496ddf482cd30a8e
Steve Gordon <sgordon@hotmail.co.uk>
2017-09-05 19:47:56 +0100

    Title

--- build.sh
+++ build.sh
@@ -4,10 +4,6 @@ set -e
 
 docker build -t 865288682694.dkr.ecr.eu-west-2.amazonaws.com/dockerdemo-servicea:${GIT_COMMIT} .
 
-sudo aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
-sudo aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-sudo aws configure set default.region eu-west-2
-
 set +x
 loginCommand=$(sudo aws ecr get-login --no-include-email --region eu-west-2)
 $loginCommand
@@ -19,4 +15,4 @@ docker rmi 865288682694.dkr.ecr.eu-west-2.amazonaws.com/dockerdemo-servicea:${GI
 sed -e "s/%GIT_SHA%/${GIT_COMMIT}/g" ./TaskDefinition.json > ./TaskDefinition-${GIT_COMMIT}.json
 sudo aws ecs register-task-definition --cli-input-json file://./TaskDefinition-${GIT_COMMIT}.json
 
-sudo aws ecs update-service --cluster dockerdemo --service DockerDemo-ECSService-3SZPUTMP2QC1 --task-definition dockerdemo-servicea
+sudo aws ecs update-service --cluster dockerdemo --service DockerDemo-ECSService-1HWPSO67M3INW --task-definition dockerdemo-servicea
