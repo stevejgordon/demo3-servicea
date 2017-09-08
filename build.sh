40c10d102ae5d6533df911a89fbe257401ac50e0
Steve Gordon <sgordon@hotmail.co.uk>
2017-09-08 08:27:11 +0100

    Title

--- build.sh
+++ build.sh
@@ -18,4 +18,4 @@ docker rmi 865288682694.dkr.ecr.eu-west-2.amazonaws.com/dockerdemo-servicea:${GI
 sed -e "s/%GIT_SHA%/${GIT_COMMIT}/g" ./TaskDefinition.json > ./TaskDefinition-${GIT_COMMIT}.json
 sudo aws ecs register-task-definition --cli-input-json file://./TaskDefinition-${GIT_COMMIT}.json
 
-sudo aws ecs update-service --cluster dockerdemo --service DockerDemo-ECSService-HQMWKTYZV23V --task-definition dockerdemo-servicea
+sudo aws ecs update-service --cluster dockerdemo --service DockerDemo-ECSService-QFMHKWI5IRE1 --task-definition dockerdemo-servicea
