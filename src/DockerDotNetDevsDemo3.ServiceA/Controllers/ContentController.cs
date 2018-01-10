55990c9bbfd14bbf4eb45ed80a51d2b0affde9e1
Steve Gordon <sgordon@hotmail.co.uk>
2018-01-10 10:22:09 +0000

    Test build before demo!

--- src/DockerDotNetDevsDemo3.ServiceA/Controllers/ContentController.cs
+++ src/DockerDotNetDevsDemo3.ServiceA/Controllers/ContentController.cs
@@ -8,7 +8,7 @@ namespace DockerDotNetDevsDemo3.ServiceA.Controllers
         [HttpGet, Route("title")]
         public string GetTitle()
         {
-            return "Hello from Docker!";
+            return "Hello from Docker!!";
         }     
     }
 }
