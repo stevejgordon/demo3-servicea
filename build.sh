7bbaee23b0beda49b3bf991f50918de464b209c5
Steve Gordon <sgordon@hotmail.co.uk>
2018-04-14 08:40:02 +0100

    sudo

--- build.sh
+++ build.sh
@@ -2,18 +2,18 @@
 
 set -e
 
-docker build -t 865288682694.dkr.ecr.eu-west-2.amazonaws.com/dockerdemo-servicea:${GIT_COMMIT} .
+sudo docker build -t 865288682694.dkr.ecr.eu-west-2.amazonaws.com/dockerdemo-servicea:${GIT_COMMIT} .
 
 sudo aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 sudo aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 sudo aws configure set default.region eu-west-2
 
 set +x
-$(sudo aws ecr get-login --no-include-email --region eu-west-2)
+sudo $(sudo aws ecr get-login --no-include-email --region eu-west-2)
 set -x
 
-docker push 865288682694.dkr.ecr.eu-west-2.amazonaws.com/dockerdemo-servicea:${GIT_COMMIT}
-docker rmi 865288682694.dkr.ecr.eu-west-2.amazonaws.com/dockerdemo-servicea:${GIT_COMMIT}
+sudo docker push 865288682694.dkr.ecr.eu-west-2.amazonaws.com/dockerdemo-servicea:${GIT_COMMIT}
+sudo docker rmi 865288682694.dkr.ecr.eu-west-2.amazonaws.com/dockerdemo-servicea:${GIT_COMMIT}
 
 sed -e "s/%GIT_SHA%/${GIT_COMMIT}/g" ./TaskDefinition.json > ./TaskDefinition-${GIT_COMMIT}.json
 sudo aws ecs register-task-definition --cli-input-json file://./TaskDefinition-${GIT_COMMIT}.json
